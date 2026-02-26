8f84d19e2142f438198441b102986344dec8a160
YoungJin Yun <yupmin@gmail.com>
2026-02-26 18:50:35 +0900

    test(cli): add swift integration fixture and tests

---
+++ tests/fixtures/swift/diagnostics_fail.swift
@@ -0,0 +1,6 @@
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+let result: Int = add(1, "2")
+print(result)
---
+++ tests/fixtures/swift/sample.swift
@@ -0,0 +1,6 @@
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+let result = add(1, 2)
+print(result)
